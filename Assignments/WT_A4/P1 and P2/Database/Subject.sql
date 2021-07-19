96246ef032c4a7864cdfd4007b66d67a42afd6a2
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-07-19 18:30:02 +0530

    Create Subject.sql

---
+++ Assignments/WT_A4/P1 and P2/Database/Subject.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[Subject] (
+    [Id]   INT          NOT NULL,
+    [Name] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (1, N'Data Structure and Algorithms')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (2, N'ASP.NET')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (3, N'C#')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (4, N'VB')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (5, N'Accounts')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (6, N'Business Studies')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (7, N'C')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (8, N'English')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (9, N'C++')
+INSERT INTO [dbo].[Subject] ([Id], [Name]) VALUES (10, N'Java')
+
